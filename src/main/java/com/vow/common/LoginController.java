a772e50aa74b8c90d4869fc63a79c33b98a5db5d
cratuz <daman.pahuja@cratuz.com>
2017-03-19 18:20:11 +0530

    first commit

---
+++ src/main/java/com/vow/common/LoginController.java
@@ -0,0 +1,16 @@
+package com.vow.common;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+@RequestMapping("/hello")
+public String hello()
+{
+
+	return "vow";
+}
+	
+}
---
+++ src/main/java/com/vow/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.vow.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+@RequestMapping("/hello")
+public String hello()
+{
+
+	return "vow";
+}
+	
+}
